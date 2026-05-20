a0a99cecaa4a46f13e87d3cba28626d4fe2aa632
Gaurav Kumar <gauravkumar94527@gmail.com>
2026-05-20 21:53:26 +0530

    to add init

---
+++ backend/app/schema/userSchema.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+
+
+class AuthProvider(str):
+    GOOGLE = "google"
+    # FACEBOOK = "facebook" not implemented yet
+    GITHUB = "github"
+    LOCAL = "local"
+
+class UserBase(BaseModel):
+    email: EmailStr
+    username: str = Field(..., min_length=3, max_length=50)
+    # provider: AuthProvider
+
+class UserCreate(UserBase):
+    password: str = Field(..., min_length=6)
+    fullname: str = Field(..., min_length=3, max_length=50)
+    bio: Optional[str] = None
+    avatar: Optional[str] = None
+    oAuth_provider: AuthProvider = AuthProvider.LOCAL
+    oAuth_provider_id: Optional[str] = None
+
+class UserLogin(BaseModel):
+    identifier: str # can be email or username
+    password: str
+    oAuth_provider: AuthProvider = AuthProvider.LOCAL
